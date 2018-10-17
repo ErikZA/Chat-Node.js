ca3df0f996826f4faeb3c5402a93d627be25afc0
ErikZambeli <erik_12_nf@live.com>
2018-10-17 19:49:39 -0300

    banco

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoClient = require('mongodb').MongoClient;
-mongoClient.connect('mongodb://localhost:27017/chatIndividual', { useNewUrlParser: true },
+mongoClient.connect('mongodb://erik:erik1234@ds251245.mlab.com:51245/chat-data', { useNewUrlParser: true },
     function (err, conn) {
         if (err) return console.log('Não conectou ao banco' + err);
         global.db = conn.db('chatIndividual');
